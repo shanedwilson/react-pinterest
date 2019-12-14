fa476c4bde8d77a5e35cddf75e2c320f733c4983
Shane D Wilson <me@shanedwilson.com>
2019-12-14 09:32:47 -0600

    auth working

--- src/helpers/data/connection.js
+++ src/helpers/data/connection.js
@@ -1,5 +1,5 @@
 import firebase from 'firebase/app';
-import firebaseConfig from './apiKeys.json';
+import firebaseConfig from '../apiKeys.json';
 
 const firebaseApp = () => {
   // check if firebase app exists.  If not create one.
